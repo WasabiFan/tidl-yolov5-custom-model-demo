77e9854c95eb526d2d03a5d1779452aac0566c9e
Kaelin Laundry <wasabifan@outlook.com>
2022-10-17 02:42:09 +0000

    Test script tweaks after running outside Docker

--- compile_model.py
+++ compile_model.py
@@ -19,8 +19,8 @@ if __name__ == "__main__":
     out_model_name = os.path.splitext(os.path.basename(model_path))[0] + "_with_shapes.onnx"
     out_model_path = os.path.join(out_dir_path, out_model_name)
     onnx.shape_inference.infer_shapes_path(model_path, out_model_path)
-    
-    artifacts_dir = os.path.join(out_dir_path, "intermediate_artifacts")
+
+    artifacts_dir = os.path.join(out_dir_path, "tidl_output")
     try:
         shutil.rmtree(artifacts_dir)
     except FileNotFoundError:
@@ -31,8 +31,9 @@ if __name__ == "__main__":
     so = rt.SessionOptions()
     
     print("Available execution providers : ", rt.get_available_providers())
-    
-    print(artifacts_dir)
+
+    num_calibration_frames = 10
+    num_calibration_iterations = 20
     compilation_options = {
         "platform": "J7",
         "version": "8.2",
@@ -42,13 +43,13 @@ if __name__ == "__main__":
 
         "tensor_bits": 8,
         # "import": "no",
-        
+
         "debug_level": 300,
-        
+
         # note: to add advanced options here, start it with 'advanced_options:'
         # example 'advanced_options:pre_batchnorm_fold':1
-        "advanced_options:calibration_frames": 1,
-        # "advanced_options:calibration_iterations": XXX,
+        "advanced_options:calibration_frames": num_calibration_frames,
+        "advanced_options:calibration_iterations": num_calibration_iterations,
     }
 
     desired_eps = ['TIDLCompilationProvider','CPUExecutionProvider']
@@ -58,7 +59,7 @@ if __name__ == "__main__":
         provider_options=[compilation_options, {}],
         sess_options=so
     )
-    
+
     input_details, = sess.get_inputs()
     batch_size, channel, height, width = input_details.shape
     print(input_details.shape)
@@ -66,22 +67,21 @@ if __name__ == "__main__":
     assert channel == 3
     input_name = input_details.name
     input_type = input_details.type
-    
+
     print(f'Input "{input_name}": {input_type}')
 
     assert input_type == 'tensor(float)'
-    
-    
-    # dummy_data = np.random.standard_normal(size = (1, channel, height, width))
-    # dummy_data = (dummy_data - np.array((0.485, 0.456, 0.406), dtype=np.single)[:, None, None]) / np.array((0.229, 0.224, 0.225), dtype=np.single)[:, None, None]
-    dummy_data = np.ones((1, channel, height, width))
 
-    dummy_data = dummy_data.astype(np.single)
-    print(dummy_data.dtype)
+    
+    for i in range(num_calibration_frames):
+        dummy_data = np.random.standard_normal(size = (1, channel, height, width))
+        dummy_data = (dummy_data - np.array((0.485, 0.456, 0.406), dtype=np.single)[:, None, None]) / np.array((0.229, 0.224, 0.225), dtype=np.single)[:, None, None]
+        # dummy_data = np.ones((1, channel, height, width))
 
-    # TODO: de-mean and normalize a proper image
+        dummy_data = dummy_data.astype(np.single)
+
+        # TODO: de-mean and normalize a proper image
+        input_data = dummy_data
+        sess.run(None, {input_name: input_data})
     
-    input_data = dummy_data
-    sess.run(None, {input_name: input_data})
-    
-    print("Exiting -- pay no attention to the SIGSEGV behind the curtain")
+    print("Compilation complete")
--- dump_sample_network.py
+++ dump_sample_network.py
@@ -10,8 +10,8 @@ from torchvision.models import resnet50
 if __name__ == "__main__":
     _, out_file = sys.argv
     
-    model = torch.nn.Sequential(torch.nn.Flatten(), torch.nn.Linear(3*224*224, 1))
-    # model = resnet50(pretrained=True)
+    # model = torch.nn.Sequential(torch.nn.Flatten(), torch.nn.Linear(3*224*224, 1))
+    model = resnet50(pretrained=True)
     model.eval()
 
     batch_size = 1
@@ -24,12 +24,12 @@ if __name__ == "__main__":
         x,                         # model input (or a tuple for multiple inputs)
         out_file,                  # where to save the model (can be a file or file-like object)
         export_params=True,        # store the trained parameter weights inside the model file
-        # opset_version=10,          # the ONNX version to export the model to
-        # do_constant_folding=True,  # whether to execute constant folding for optimization
+        opset_version=10,          # the ONNX version to export the model to
+        do_constant_folding=True,  # whether to execute constant folding for optimization
         input_names = ['input'],   # the model's input names
         output_names = ['output'], # the model's output names
-        # dynamic_axes={'input' : {0 : 'batch_size'},    # variable length axes
-        #               'output' : {0 : 'batch_size'}}
+        dynamic_axes={'input' : {0 : 'batch_size'},    # variable length axes
+                      'output' : {0 : 'batch_size'}}
         )
 
     import onnx
